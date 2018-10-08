4e649de17773a2d54affe7bb2847ce60cc484420
へーさん <hira9603859504@gmail.com>
2018-10-09 02:23:04 +0900

    👊

--- 1/sketch.js
+++ 1/sketch.js
@@ -1,21 +1,35 @@
+var fontSize = 10;
+var r = 18;
+var c = ["👊", "👊🏻", "👊🏼", "👊🏽", "👊🏾", "👊🏿"];
+
+var nX, nY;
+var arr = [];
+
 function setup () {
   createCanvas(windowWidth, windowHeight);
+  textSize(fontSize);
+  nX = int(windowWidth / (fontSize * 1.1));
+  nY = int(windowHeight / (fontSize * 1.1));
+  for (var x = 0; x < nX; x++) {
+    for (var y = 0; y < nY; y++) {
+      arr.push(0);
+    }
+  }
 }
 
 function draw () {
   background(255);
-  var fontSize = 10;
-  textSize(fontSize);
-  for (var x = 1; x < windowWidth; x += fontSize+1) {
-    for (var y = 1; y < windowHeight; y += fontSize+1) { 
-      var dx = abs(x - mouseX);
-      var dy = abs(y - mouseY);
-      if (dx * dx + dy * dy < 8 * fontSize * fontSize) {
-        text("🌕", x, y);
-      } else {
-        text("🌑", x, y);
+  for (var x = 1; x <= nX; x++) {
+    for (var y = 1; y <= nY; y++) {
+      var dx = abs(x * fontSize * 1.1 - mouseX);
+      var dy = abs(y * fontSize * 1.1 - mouseY);
+      var d = (dx * dx + dy * dy) / (fontSize * fontSize * 1.1 * 1.1);
+      if (d <= r) {
+        arr[x + nX * y] = 5;
       }
+      text(c[int(arr[x + nX * y])], x * fontSize * 1.1, y * fontSize * 1.1);
+      arr[x + nX * y] = arr[x + nX * y] * 0.9;
     }
   }
-  
+
 }
